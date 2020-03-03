27e023845a6dcecbe86d2265dc1c175da52f9f65
nireo <eemil.lehtikangas1@gmail.com>
2020-03-03 08:21:45 +0200

    added board item component

--- client/src/components/board/Item.tsx
+++ client/src/components/board/Item.tsx
@@ -1,5 +1,45 @@
-import React from 'react';
+import * as React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
 
-export const Item: React.FC = () => {
-  return <div></div>;
+type BoardItemProps = {
+  index: number;
+  item: any;
+};
+
+type BoardItemStylesProps = {
+  isDragging: boolean;
+};
+
+const BoardItemEl = styled.div<BoardItemStylesProps>`
+  padding: 8px;
+  background-color: ${props => (props.isDragging ? '#d3e4ee' : '#fff')};
+  border-radius: 4px;
+  transition: background-color 0.25s ease-out;
+
+  &:hover {
+    background-color: #f7fafc;
+  }
+
+  & + & {
+    margin-top: 4px;
+  }
+`;
+
+// Create and export the BoardItem component
+export const BoardItem = (props: BoardItemProps) => {
+  return (
+    <Draggable draggableId={props.item.id} index={props.index}>
+      {(provided, snapshot) => (
+        <BoardItemEl
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
+        >
+          {props.item.content}
+        </BoardItemEl>
+      )}
+    </Draggable>
+  );
 };
