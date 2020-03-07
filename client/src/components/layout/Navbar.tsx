1e66b0ff11aa9a71dc711f1f9d114d9e03935b4a
nireo <eemil.lehtikangas1@gmail.com>
2020-03-07 17:45:21 +0200

    changed up the rendering a bit

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -2,48 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Nav = styled.div`
-  background-color: #fff;
+  background-color: #0079bf;
+  color: #172b4d;
   border-bottom: 1px solid rgba(0, 0, 0, 0.2);
   margin-bottom: 1rem;
+  height: 40px;
 `;
 
 const NavHeader = styled.div`
   max-width: 1010px;
-  padding: 16px 20px;
   width: 100%;
   display: flex;
-  align-items: center;
-  margin: 0 auto;
-`;
-
-const NavLeft = styled.div`
-  /* 1/3 */
-  width: 33.333%;
-  text-align: center;
-`;
-
-const NavCenter = styled.div`
-  width: 33.333%;
-  text-align: center;
-`;
-
-const NavRight = styled.div`
-  width: 33.333%;
-  text-align: right;
 `;
 
 export const Navbar: React.FC = () => {
-  return (
-    <Nav>
-      <NavHeader>
-        <NavLeft>Gello</NavLeft>
-        <NavCenter></NavCenter>
-        <NavRight>
-          <a className="link link-hover" href="/signup">
-            Sign up
-          </a>
-        </NavRight>
-      </NavHeader>
-    </Nav>
-  );
+  return <div></div>;
 };
--- client/src/components/manage/Main.tsx
+++ client/src/components/manage/Main.tsx
@@ -7,6 +7,12 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import PermIdentityOutlinedIcon from '@material-ui/icons/PermIdentityOutlined';
 import Icon from '@material-ui/core/Icon';
+import { Link } from 'react-router-dom';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import Card from '@material-ui/core/Card';
+import CarContent from '@material-ui/core/CardContent';
+import CardContent from '@material-ui/core/CardContent';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 type Props = {
   dispatch: any;
@@ -21,14 +27,24 @@ const ManageMain: React.FC<Props> = ({ boards, dispatch }) => {
       setLoaded(false);
     }
   }, []);
-  console.log(boards);
 
   return (
     <Container maxWidth="lg">
       <Grid container>
         <Grid item xs={3}>
-          <h2>Welcome user</h2>
-          <button className="button">Create new board</button>
+          <nav>
+            <ul>
+              <li style={{ listStyle: 'none ' }}>
+                <Link to="/home" className="nav-link">
+                  <Icon style={{ marginRight: '0.5rem' }}>
+                    <DashboardIcon />
+                  </Icon>
+                  {'  '}
+                  <strong>Boards</strong>
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </Grid>
         <Grid item xs={9}>
           <div style={{ display: 'flex' }}>
@@ -37,9 +53,33 @@ const ManageMain: React.FC<Props> = ({ boards, dispatch }) => {
             </Icon>
             <h3>Personal boards</h3>
           </div>
-          {boards.map((board: Board) => (
-            <div>{board.title}</div>
-          ))}
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            {boards.map((board: Board) => (
+              <Card
+                style={{
+                  height: '80px',
+                  marginRight: '1rem',
+                  border: 'none'
+                }}
+              >
+                <div style={{ paddingLeft: '0.5rem', paddingRight: '4rem' }}>
+                  <h4 style={{ margin: 0, padding: 0 }}>{board.title}</h4>
+                  <Icon>
+                    <StarBorderIcon />
+                  </Icon>
+                </div>
+              </Card>
+            ))}
+            <button
+              style={{ border: 'none', background: 'none', cursor: 'pointer' }}
+            >
+              <div className="create-board-button">
+                <p style={{ marginLeft: '1rem', marginRight: '1rem' }}>
+                  Create new board
+                </p>
+              </div>
+            </button>
+          </div>
         </Grid>
       </Grid>
     </Container>
