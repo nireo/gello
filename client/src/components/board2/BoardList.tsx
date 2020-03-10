9a4f8783a7eda1e25b9757d84a9274c46e92d3da
nireo <eemil.lehtikangas1@gmail.com>
2020-03-10 16:24:06 +0200

    added a popover menu for lists

--- client/src/components/board2/BoardList.tsx
+++ client/src/components/board2/BoardList.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BoardCard } from './BoardCard';
 import ActionButton from './ActionButton';
 import { Droppable, Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import IconButton from '@material-ui/core/IconButton';
+import Popover from '@material-ui/core/Popover';
 
 const ListContainer = styled.div`
   background-color: #dfe3e6;
@@ -21,6 +24,19 @@ type Props = {
 };
 
 export const BoardList: React.FC<Props> = ({ title, items, id, index }) => {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const componentId = open ? 'menu-popover' : undefined;
+
   return (
     <Draggable draggableId={String(id)} index={index}>
       {provided => (
@@ -32,7 +48,34 @@ export const BoardList: React.FC<Props> = ({ title, items, id, index }) => {
           <Droppable droppableId={String(id)}>
             {provided => (
               <div {...provided.droppableProps} ref={provided.innerRef}>
-                <h4>{title}</h4>
+                <div style={{ display: 'flex' }}>
+                  <h4>{title}</h4>
+                  <IconButton
+                    style={{ marginLeft: 'auto' }}
+                    size="small"
+                    disableFocusRipple={true}
+                    aria-descripedby={id}
+                    onClick={handleClick}
+                  >
+                    <MoreHorizIcon />
+                  </IconButton>
+                </div>
+                <Popover
+                  id={componentId}
+                  open={open}
+                  onClose={handleClose}
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center'
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center'
+                  }}
+                >
+                  Hello
+                </Popover>
                 {items.map((item: any, index: number) => (
                   <BoardCard
                     key={item.uuid}
