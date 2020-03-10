41e7a36da7f3ba50470deeead16fe767fd6701d9
nireo <eemil.lehtikangas1@gmail.com>
2020-03-10 16:42:11 +0200

    added a list menu

--- client/src/components/board2/BoardList.tsx
+++ client/src/components/board2/BoardList.tsx
@@ -6,6 +6,12 @@ import styled from 'styled-components';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import IconButton from '@material-ui/core/IconButton';
 import Popover from '@material-ui/core/Popover';
+import { makeStyles, Theme, createStyles } from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import Divider from '@material-ui/core/Divider';
 
 const ListContainer = styled.div`
   background-color: #dfe3e6;
@@ -23,7 +29,18 @@ type Props = {
   index: number;
 };
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper
+    }
+  })
+);
+
 export const BoardList: React.FC<Props> = ({ title, items, id, index }) => {
+  const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -74,7 +91,32 @@ export const BoardList: React.FC<Props> = ({ title, items, id, index }) => {
                     horizontal: 'center'
                   }}
                 >
-                  Hello
+                  <div>
+                    <List
+                      component="nav"
+                      aria-labelledby="list-menu"
+                      subheader={
+                        <ListSubheader component="div">
+                          List actions
+                        </ListSubheader>
+                      }
+                      className={classes.root}
+                    >
+                      <ListItem button style={{ width: '100%' }}>
+                        <ListItemText primary="Add card..." />
+                      </ListItem>
+                      <ListItem button>
+                        <ListItemText primary="Copy list..." />
+                      </ListItem>
+                      <ListItem button>
+                        <ListItemText primary="Move list..." />
+                      </ListItem>
+                      <Divider />
+                      <ListItem button>
+                        <ListItemText primary="Sort by..." />
+                      </ListItem>
+                    </List>
+                  </div>
                 </Popover>
                 {items.map((item: any, index: number) => (
                   <BoardCard
