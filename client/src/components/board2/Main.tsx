2eb0237d911fbf40cfa124747cc6f1d38fe8c066
nireo <eemil.lehtikangas1@gmail.com>
2020-03-17 18:16:57 +0200

    made changing the board title possiable

--- client/src/components/board2/Main.tsx
+++ client/src/components/board2/Main.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { BoardList } from './BoardList';
 import { connect } from 'react-redux';
 import { AppState } from '../../store';
 import ActionButton from './ActionButton';
 import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
-import { sort, initListData } from '../../actions';
+import { sort, initListData, updateActive } from '../../actions';
 import styled from 'styled-components';
 import Drawer from '@material-ui/core/Drawer';
 import { Theme, withStyles } from '@material-ui/core/styles';
@@ -13,6 +13,7 @@ import CloseIcon from '@material-ui/icons/Close';
 import { DrawerContent } from './Drawer';
 import { setActiveBoard } from '../../actions';
 import { getSingleBoard } from '../../services/board';
+import { CreateBoard } from '../../interfaces/Board';
 
 const drawerWidth = 339;
 
@@ -42,7 +43,21 @@ type Props = {
 class Main extends React.Component<Props> {
   state = {
     loaded: false,
-    open: false
+    open: false,
+    newTitle: '',
+    showTitleForm: false
+  };
+
+  showTitleForm = () => {
+    this.setState({
+      showTitleForm: true
+    });
+  };
+
+  closeTitleForm = () => {
+    this.setState({
+      showTitleForm: false
+    });
   };
 
   onDragEnd = (result: DropResult) => {
@@ -86,6 +101,16 @@ class Main extends React.Component<Props> {
     });
   };
 
+  updateBoard = (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newData: CreateBoard = {
+      title: this.state.newTitle,
+      color: this.props.active.color
+    };
+
+    this.props.dispatch(updateActive(this.props.active, newData));
+  };
+
   render() {
     const { lists, active } = this.props;
     const { classes } = this.props;
@@ -102,7 +127,34 @@ class Main extends React.Component<Props> {
         <DragDropContext onDragEnd={this.onDragEnd}>
           <div>
             <div style={{ display: 'flex' }}>
-              <h2 style={{ color: 'white' }}>{active.title}</h2>
+              {this.state.showTitleForm ? (
+                <h2>
+                  <form onSubmit={this.updateBoard}>
+                    <input
+                      autoFocus
+                      onBlur={this.closeTitleForm}
+                      value={this.state.newTitle}
+                      onChange={({ target }) =>
+                        this.setState({
+                          newTitle: target.value
+                        })
+                      }
+                      style={{
+                        background: 'none',
+                        border: 'none',
+                        color: 'white',
+                        fontSize: '1.17em',
+                        fontFamily: 'Open Sans, sans-serif'
+                      }}
+                    />
+                    <button type="submit" style={{ display: 'none' }}></button>
+                  </form>
+                </h2>
+              ) : (
+                <h2 style={{ color: 'white' }} onClick={this.showTitleForm}>
+                  {active.title}
+                </h2>
+              )}
               <div style={{ float: 'right' }}>
                 <button onClick={this.handleOpen}>Open drawer</button>
               </div>
