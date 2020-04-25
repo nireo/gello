b65d4b32a60e556e34036627a5d503e3a51511b4
nireo <eemil.lehtikangas1@gmail.com>
2020-04-25 12:55:29 +0300

    added more acitons and reducer functions

--- client/src/actions/templateAction.ts
+++ client/src/actions/templateAction.ts
@@ -1,6 +1,10 @@
 import { CreateTemplate } from '../interfaces/Template';
 import { Dispatch } from 'redux';
-import { createTemplate } from '../services/template';
+import {
+  createTemplate,
+  getTemplates,
+  deleteTemplate,
+} from '../services/template';
 import { CONSTANTS } from './index';
 
 export const createTemplateAction = (newTemplate: CreateTemplate) => {
@@ -9,6 +13,26 @@ export const createTemplateAction = (newTemplate: CreateTemplate) => {
     dispatch({
       type: CONSTANTS.CREATE_TEMPLATE,
       data: createdTemplate,
+    });
+  };
+};
+
+export const getTemplatesAction = () => {
+  return async (dispatch: Dispatch) => {
+    const templates = await getTemplates();
+    dispatch({
+      type: CONSTANTS.GET_TEMPLATES,
+      data: templates,
+    });
+  };
+};
+
+export const deleteTemplateAction = (id: string) => {
+  return async (dispatch: Dispatch) => {
+    await deleteTemplate(id);
+    dispatch({
+      type: CONSTANTS.DELETE_TEMPLATE,
+      id: id,
     });
   };
 };
--- client/src/store/templates/index.ts
+++ client/src/store/templates/index.ts
@@ -1,3 +1,5 @@
+import { CONSTANTS } from '../../actions';
+
 const initialState = {
   userTemplates: [],
   templates: [],
@@ -5,6 +7,14 @@ const initialState = {
 
 const reducer = (state: any = initialState, action: any) => {
   switch (action.type) {
+    case CONSTANTS.GET_TEMPLATES:
+      return action.data;
+    case CONSTANTS.CREATE_TEMPLATE:
+      let newState = state;
+      newState.userTemplates = [...newState.userTemplates, action.data];
+      newState.templates = [...newState.templates, action.data];
+
+      return newState;
     default:
       return state;
   }
