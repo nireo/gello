e2fe43e24fbf5a44e2ee9efecf64d7c5e4f0f5cb
nireo <eemil.lehtikangas1@gmail.com>
2020-05-31 22:54:36 +0300

    added create activity report functions

--- database/models/activity.go
+++ database/models/activity.go
@@ -1,9 +1,13 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	"github.com/nireo/gello/lib/common"
 )
+
+var messageTypes = [4]string{"deleteItem", "deleteList", "addList", "addItem"}
 
 // Activity model
 type Activity struct {
@@ -14,6 +18,48 @@ type Activity struct {
 	// for the activity display just showing the username is fine.
 	Username string
 	Content  string
+}
+
+// CreateActivityReport creates a activity entry with username, actionType and boardID
+// and returns a status on if the creation was successful
+func CreateActivityReport(username, actionType, boardID string, db *gorm.DB) bool {
+	// finding the board could be handeled in the controllers, but searching for boards
+	// in a item controller isn't really consistent in my opinion.
+	var board Board
+	if err := db.Where("uuid = ?", boardID).First(&board).Error; err != nil {
+		return false
+	}
+
+	contentMessage, ok := generateActivityMessage(username, actionType)
+	if !ok {
+		return false
+	}
+
+	newActivity := Activity{
+		UUID:     common.GenerateUUID(),
+		Username: username,
+		Content:  contentMessage,
+	}
+
+	db.NewRecord(newActivity)
+	db.Save(&newActivity)
+
+	return true
+}
+
+func generateActivityMessage(username, actionType string) (string, bool) {
+	switch actionType {
+	case messageTypes[0]:
+		return fmt.Sprintf("%s has deleted an item.", username), true
+	case messageTypes[1]:
+		return fmt.Sprintf("%s has deleted a list", username), true
+	case messageTypes[2]:
+		return fmt.Sprintf("%s has created a list", username), true
+	case messageTypes[3]:
+		return fmt.Sprintf("%s has created an item", username), true
+	default:
+		return "", false
+	}
 }
 
 // Serialize activity data into JSON-format
