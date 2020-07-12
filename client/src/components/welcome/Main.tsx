3d09788a21bbf7b8edf61af269aa825aaa6081ba
nireo <eemil.lehtikangas1@gmail.com>
2020-07-12 13:17:26 +0300

    added more fields to the welcome page

--- client/src/components/welcome/Main.tsx
+++ client/src/components/welcome/Main.tsx
@@ -21,11 +21,35 @@ export const Main: React.FC = () => {
           </div>
         </Container>
       </div>
-      <Container maxWidth="md" style={{ marginTop: '4rem' }}>
+      <Container maxWidth="lg" style={{ marginTop: '4rem' }}>
         <Typography variant="h4">Collaborate with others!</Typography>
         <Typography style={{ fontSize: '18px' }}>
           Gello makes it easier to add multiple users to a board for
-          collaboration.
+          collaboration. Also different tags can be added to a board so that
+          everyone knows what you're doing.
+        </Typography>
+        <Typography variant="h4" style={{ marginTop: '8rem' }}>
+          A productive way of working!
+        </Typography>
+        <Typography style={{ fontSize: '18px' }}>
+          With lots of different settings gello let's you configure your
+          experience fully. From different lists to colors; gello let's you edit
+          them all!
+        </Typography>
+        <Typography variant="h4" style={{ marginTop: '8rem' }}>
+          Open source
+        </Typography>
+        <Typography style={{ fontSize: '18px' }}>
+          All of gello's source code can be found on{' '}
+          <a
+            href="https://github.com/nireo/gello"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: 'none', color: '#0079b9' }}
+          >
+            github
+          </a>
+          .
         </Typography>
       </Container>
       <Footer />
